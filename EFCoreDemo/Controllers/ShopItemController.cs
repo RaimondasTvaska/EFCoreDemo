0adc3c537130cc15c102cd60e6991f3c714b2eca
RaimondasTvaska <rtvaska@gmail.com>
2021-09-15 23:59:51 +0300

    Version with added filtering

--- EFCoreDemo/Controllers/ShopItemController.cs
+++ EFCoreDemo/Controllers/ShopItemController.cs
@@ -1,6 +1,8 @@
 ﻿using EFCoreDemo.Data;
 using EFCoreDemo.Dtos;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 namespace EFCoreDemo.Controllers
 {
@@ -13,9 +15,35 @@ namespace EFCoreDemo.Controllers
             _context = context;
         }
 
+        public IActionResult Index()
+        {
+            var items = _context.ShopItems.Include(s => s.Shop).ToList();
+            var allShops = _context.Shops.ToList();
+
+            var dto = new ShopItemIndex()
+            {
+                ShopItems = items,
+                Shops = allShops
+            };
+            return View(dto);
+        }
+        [HttpPost]
+        public IActionResult Index(ShopItemIndex model)
+        {
+            var items = _context.ShopItems.Include(s => s.Shop).ToList();
+            var allShops = _context.Shops.ToList();
+
+            var dto = new ShopItemIndex()
+            {
+                ShopItems = items.Where(i => i.ShopId == model.ShopId || model.ShopId == 0).ToList(),
+                Shops = allShops
+            };
+            return View(dto);
+        }
         public IActionResult Add()
         {
             var shopItem = new ShopItemCreate();
+            shopItem.Shops = _context.Shops.ToList();
             return View(shopItem);
         }
         [HttpPost]
--- EFCoreDemo/Dtos/ShopItemIndex.cs
+++ EFCoreDemo/Dtos/ShopItemIndex.cs
@@ -1,16 +1,18 @@
 ﻿using EFCoreDemo.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace EFCoreDemo.Dtos
 {
-    public class ShopItemCreate
+    public class ShopItemIndex
     {
-        public string Name { get; set; }
-        public List<Shop> Shops { get; set; }
-
         [DisplayName("Shop")]
         public int ShopId { get; set; }
+        public List<ShopItem> ShopItems { get; set; }
+        public List<Shop> Shops { get; set; }
 
     }
 }
